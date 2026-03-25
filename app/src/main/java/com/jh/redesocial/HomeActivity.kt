ced900037245192de7d01e646cc12ad47fe72b05
joaoh19 <correareisjoaohenrique@gmail.com>
2026-03-25 10:58:17 -0300

    Resolvendo erro Home

--- app/src/main/java/com/jh/redesocial/HomeActivity.kt
+++ app/src/main/java/com/jh/redesocial/HomeActivity.kt
@@ -3,29 +3,38 @@ package com.jh.redesocial
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.firestore.firestore
+import com.google.firebase.Firebase
+import com.jh.redesocial.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val firebaseAuth = FirebaseAuth.getInstance()
         val email = firebaseAuth.currentUser!!.email.toString()
+
         val db = Firebase.firestore
         db.collection("usuarios").document(email).get()
             .addOnCompleteListener { task ->
-                if (task.isSuccessful){
+                if (task.isSuccessful) {
                     val document = task.result
+
                     val imageString = document.data!!["fotoPerfil"].toString()
                     val bitmap = Base64Converter.stringToBitmap(imageString)
+
                     binding.imgLogo.setImageBitmap(bitmap)
                     binding.txtUsername.text = document.data!!["username"].toString()
                     binding.txtNomeCompleto.text = document.data!!["nomeCompleto"].toString()
 
-
-        }
+                }
+            }
     }
+}
